207a8eb1f5e6d1137cc6424114c3eff66787bc4a
jfikents <jfikents@student.42Heilbron.de>
2023-10-06 20:47:46 +0200

    Added ft_bzero.c

---
+++ ft_bzero.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 20:23:00 by JFikents          #+#    #+#             */
+/*   Updated: 2023/10/06 20:47:08 by JFikents         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_bzero(void *address, int size)
+{
+	if (size <= 0)
+		return ;
+	else
+	{
+		*(unsigned char *)address = 0;
+		ft_bzero(address + 1, size - 1);
+		return ;
+	}
+}
+
+// int	main(void)
+// {
+// 	char str[50] = "GeeksForGeeks is for programming geeks.";
+
+// 	printf("Before memset():	%s\n", str);
+// 	ft_bzero(str, 1);
+// 	printf("After memset():		%s", str);
+// 	return (0);
+// }
