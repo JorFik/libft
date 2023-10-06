1b8f6a8c2e86e5c7ebe62e194f9785cdd1739c1c
jfikents <jfikents@student.42Heilbron.de>
2023-10-06 12:52:18 +0200

    primera prueba alpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,11 +6,25 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 12:16:17 by JFikents          #+#    #+#             */
-/*   Updated: 2023/10/06 12:27:59 by JFikents         ###   ########.fr       */
+/*   Updated: 2023/10/06 12:51:43 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalpha(char c)
+#include <stdlib.h>
+
+int	ft_isalpha(char car)
 {
-    
+	if ((car >= 101 && car <= 132) || (car >= 141 && car <= 172))
+		return (1);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+		return (0);
+	if (ft_isalpha (argv[1][1]))
+		printf("Es Alpha");
+	if (!ft_isalpha (argv[1][1]))
+		printf("No es Alpha");
 }
