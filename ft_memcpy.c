2283a001062d312ea4addf6dec675ef229a894e5
jfikents <jfikents@student.42Heilbron.de>
2023-10-07 19:52:28 +0200

    simplified version of memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 20:49:04 by JFikents          #+#    #+#             */
-/*   Updated: 2023/10/06 21:43:46 by JFikents         ###   ########.fr       */
+/*   Updated: 2023/10/07 19:50:25 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,24 @@ void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t size)
 	int	i;
 
 	i = 0;
-	while (size > 0)
+	if (!src)
+		return (src);
+	while (size --)
 	{
-		if (*(unsigned char *)(src + i) != '\0')
-			*(unsigned char *)(dest + i) = *(unsigned char *)(src + i);
-		else
-		{
-			*(unsigned char *)(dest + i) = '\0';
-			return (dest);
-		}
-		size --;
+		*(char *)(dest + i) = *(char *)(src + i);
 		i ++;
 	}
 	return (dest);
 }
 
+// *(char *)(dest + size) = *(char *)(src + size);
 // int	main(void)
 // {
-// 	char		src[50] = "https://www.tutorialspoint.com";
+// 	char		src[50] = "Hasta la Proxima";
 // 	char		dest[50] = "Heloooo!!";
 
-// 	printf("Before memcpy dest = %s\n", src);
-// 	ft_memcpy(src, dest, 50);
+// 	printf("Before memcpy dest = %s\n", dest);
+// 	ft_memcpy(dest, src, 50);
 // 	printf("After memcpy dest = %s\n", dest);
 
 // 	return (0);
