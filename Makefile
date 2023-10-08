e69e238d8fd319f221a63139fd3526c28a4d3c74
jfikents <jfikents@student.42Heilbron.de>
2023-10-08 21:53:40 +0200

    Added empty toupper and updated libft/Make

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ NAME = libft.a
 
 ft_so_far = ft_bzero.c ft_strlen.c ft_memset.c ft_memmove.c \
 ft_memcpy.c ft_isprint.c ft_isdigit.c ft_isascii.c ft_isalpha.c\
-ft_isalnum.c ft_strlcpy.c ft_strlcat.c
+ft_isalnum.c ft_strlcpy.c ft_strlcat.c ft_toupper.c
 h_file = libft.h
 
 .PHONY: clean fclean re all
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 22:47:39 by JFikents          #+#    #+#             */
-/*   Updated: 2023/10/08 20:19:57 by JFikents         ###   ########.fr       */
+/*   Updated: 2023/10/08 21:52:16 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void			*ft_memset(void *address, int filler, unsigned long size);
 void			*ft_memmove(void *dst, const void *src, unsigned long len);
 void			*ft_memcpy(void *dest, const void *src, unsigned long size);
 void			ft_bzero(void *address, unsigned long size);
+int				ft_toupper(int c);
 int				ft_isprint(unsigned char car);
 int				ft_isdigit(unsigned char car);
 int				ft_isascii(unsigned char car);
