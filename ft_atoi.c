9dcff4a050f10793c747a6cd3a3a3071760459a1
jfikents <jfikents@student.42Heilbron.de>
2023-10-10 20:20:08 +0200

    + w atoi, u H/M file

---
+++ ft_atoi.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 18:48:16 by JFikents          #+#    #+#             */
+/*   Updated: 2023/10/10 20:18:29 by JFikents         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_jmpspc(char *s)
+{
+	while ((*s == ' ' || (*s >= 9 && *s <= 13)) && *s)
+		s++;
+	return (s);
+}
+
+char	*ft_jmpsign(char *s)
+{
+	if ((*s == '-' || *s == '+') && *s)
+		s ++;
+	return (s);
+}
+
+int	ft_sign(char *s)
+{
+	int	counter;
+
+	counter = 0;
+	while ((*s == ' ' || *s == '-' || *s == '+' || (*s >= 9 && *s <= 13)) && *s)
+	{
+		if (*s == ' ' || *s == '+' || (*s >= 9 && *s <= 13))
+			s++;
+		if (*s == '-')
+			counter++;
+		if (*s == '-')
+			s++;
+	}
+	if (counter % 2)
+		return (-1);
+	return (1);
+}
+
+int	ft_atoi(const char *str)
+{
+	char	*s;
+	int		new_n_shiny_usable_int;
+	int		sign;
+	int		i;
+
+	i = 0;
+	new_n_shiny_usable_int = 0;
+	sign = ft_sign((char *)str);
+	s = ft_jmpsign(ft_jmpspc((char *)str));
+	while (s[i] && s[i] >= 48 && s[i] <= 57)
+	{
+		new_n_shiny_usable_int *= 10;
+		new_n_shiny_usable_int += (int)s[i] - 48;
+		i++;
+	}
+	return (new_n_shiny_usable_int * sign);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 22:47:39 by JFikents          #+#    #+#             */
-/*   Updated: 2023/10/09 23:52:18 by JFikents         ###   ########.fr       */
+/*   Updated: 2023/10/10 20:01:39 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ void			ft_bzero(void *address, unsigned long size);
 void			*ft_memset(void *address, int filler, unsigned long size);
 void			*ft_memmove(void *dst, const void *src, unsigned long len);
 void			*ft_memcpy(void *dest, const void *src, unsigned long size);
-void			*memchr(void *s, int c, unsigned long n);
+void			*ft_memchr(void *s, int c, unsigned long n);
 char			*ft_strchr(char *src, int c);
 char			*ft_strrchr(char *src, int c);
 char			*ft_strnstr(const char *haystack,
 					const char *needle, unsigned long len);
-int				ft_memcmp(const void *s1, const void *s2, unsigned long n);
+int				ft_atoi(const char *str);
 int				ft_toupper(int c);
 int				ft_isprint(int car);
 int				ft_isdigit(int car);
@@ -30,6 +30,7 @@ int				ft_isascii(int car);
 int				ft_isalpha(int car);
 int				ft_isalnum(int car);
 int				ft_tolower(int c);
+int				ft_memcmp(const void *s1, const void *s2, unsigned long n);
 int				ft_strncmp(const char *s1, const char *s2, unsigned long n);
 unsigned long	ft_strlen(const char *str);
 unsigned long	ft_strlcpy(char *dst, const char *src, unsigned long dst_size);
