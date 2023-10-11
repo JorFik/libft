7819c0c06965c03a684dced4a08c29e63e5b91d8
jfikents <jfikents@student.42Heilbron.de>
2023-10-11 13:16:18 +0200

    c memcpy case dst && src == NULL, c strnstr case n>h

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 20:49:04 by JFikents          #+#    #+#             */
-/*   Updated: 2023/10/10 21:56:14 by JFikents         ###   ########.fr       */
+/*   Updated: 2023/10/10 23:09:22 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, unsigned long size)
 	int	i;
 
 	i = 0;
+	if (dest == NULL && src == NULL)
+		return (NULL);
 	while (size --)
 	{
 		*(char *)(dest + i) = *(char *)(src + i);
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 23:45:18 by JFikents          #+#    #+#             */
-/*   Updated: 2023/10/10 18:46:31 by JFikents         ###   ########.fr       */
+/*   Updated: 2023/10/11 13:12:27 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,28 +22,27 @@ unsigned long	ft_strln(const char *str)
 
 char	*ft_strnstr(const char *haystack, const char *needle, unsigned long len)
 {
-	unsigned long	s_needle;
 	unsigned long	match;
 	unsigned long	i;
 	char			*h;
 	char			*n;
 
-	s_needle = ft_strln(needle);
 	h = (char *) haystack;
 	n = (char *) needle;
 	i = 0;
-	if (s_needle == 0)
+	if (ft_strln(needle) == 0 || h == n)
 		return (h);
-	while (i < len - 1)
+	while (i < len && len > 0 && *h != '\0' && !(ft_strln(n) > ft_strln(h)))
 	{
 		match = 0;
-		while (h[i] == n[match] && i < len && h[i] && n[match])
+		while (h[i] == n[match] && i < len && (h[i] || n[match]))
 		{
 			match ++;
 			i ++;
 		}
-		if (match == s_needle)
-			return (&h[i - s_needle]);
+		if (match == ft_strln(needle))
+			return (&h[i - ft_strln(needle)]);
+		i -= match;
 		i ++;
 	}
 	return (0);
