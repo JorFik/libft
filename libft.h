3070c7c28b4ed92bc77a9c15b28473ff8f00e743
jfikents <jfikents@student.42Heilbron.de>
2023-10-17 17:15:29 +0200

    a w lstlast, u h/m file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 22:47:39 by JFikents          #+#    #+#             */
-/*   Updated: 2023/10/17 16:55:18 by JFikents         ###   ########.fr       */
+/*   Updated: 2023/10/17 17:06:00 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,4 +66,5 @@ void			ft_putnbr_fd(int n, int fd);
 t_list			*ft_lstnew(void *content);
 void			ft_lstadd_front(t_list **lst, t_list *new);
 int				ft_lstsize(t_list *lst);
+t_list			*ft_lstlast(t_list *lst);
 #endif
