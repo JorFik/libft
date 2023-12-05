6833cff0e7d93f86da5c5d75a21a0e582a9b6211
jfikents <jfikents@student.42Heilbron.de>
2023-12-05 17:46:49 +0100

    updated get_next_line to not repeat functions

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 11:48:02 by JFikents          #+#    #+#             */
-/*   Updated: 2023/11/15 16:21:30 by JFikents         ###   ########.fr       */
+/*   Updated: 2023/12/05 17:43:52 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,51 +25,6 @@ long	ft_back2zero(char **pos, char *last_read)
 		*pos = NULL;
 	}
 	return (0);
-}
-
-unsigned long	ft_strlen(const char *str)
-{
-	int	counter;
-
-	counter = 0;
-	if (str)
-	{
-		while (*(str + counter) != '\0')
-			counter ++;
-	}
-	return (counter);
-}
-
-void	*ft_calloc(unsigned long count, unsigned long size)
-{
-	void			*new_p;
-	unsigned long	i;
-
-	i = 0;
-	new_p = malloc(count * size);
-	if (new_p == NULL)
-		return (NULL);
-	while ((count * size) > i)
-	{
-		((char *)new_p)[i] = 0;
-		i ++;
-	}
-	return (new_p);
-}
-
-char	*ft_strchr(char *src, int c)
-{
-	int		i;
-	char	busco;
-
-	busco = (char) c;
-	i = 0;
-	while (src[i] != busco && src[i])
-		i ++;
-	if (src[i] == busco)
-		return (&src[i]);
-	else
-		return (0);
 }
 
 char	*ft_handle_nl(char *pre_line, char *last_read, int check_read)
