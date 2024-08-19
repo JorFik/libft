6cbc1600f589c148c6cf101f304778ca64bff694
jorFik <jfikents@student.42Heilbronn.de>
2024-08-19 10:32:00 +0200

    Fix (missed reference)

--- src/mem/mem_operations/ft_bzero.c
+++ src/mem/mem_operations/ft_bzero.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: JFikents <JFikents@student.42Heilbronn.de> +#+  +:+       +#+        */
+/*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 20:23:00 by JFikents          #+#    #+#             */
-/*   Updated: 2023/10/08 17:19:48 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/08/19 10:31:15 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,3 @@ void	ft_bzero(void *address, unsigned long size)
 {
 	ft_memset(address, 0, size);
 }
-
-// int	main(void)
-// {
-// 	char str[50] = "GeeksForGeeks is for programming geeks.";
-
-// 	printf("Before memset():	%s\n", str);
-// 	ft_bzero(str + 13, 1);
-// 	printf("After memset():		%s", str);
-// 	return (0);
-// }
